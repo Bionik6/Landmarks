74b806b5b1621b7a7025eb715cbb0d4c20d90c29
Ibrahima Ciss <bionik6@gmail.com>
2019-10-28 09:16:22 +0000

    Add Landmark list and navigation to details view

---
+++ Landmarks/LandmarkRow.swift
@@ -0,0 +1,21 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Ibrahima Ciss on 28/10/19.
+//  Copyright © 2019 Ibrahima Ciss. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow()
+    }
+}
---
+++ Landmarks/Models/Landmark.swift
@@ -0,0 +1,9 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Ibrahima Ciss on 28/10/19.
+//  Copyright © 2019 Ibrahima Ciss. All rights reserved.
+//
+
+import Foundation
