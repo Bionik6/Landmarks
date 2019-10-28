8eaebfd4bc19be9adc8c737da098477d68325c49
Ibrahima Ciss <bionik6@gmail.com>
2019-10-28 07:31:15 +0000

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Ibrahima Ciss on 28/10/19.
+//  Copyright © 2019 Ibrahima Ciss. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
